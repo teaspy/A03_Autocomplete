0814cb3c941293c38a95f703170d4db2b3caf09a
Jason Bruderer <Lastrellik@gmail.com>
2016-10-12 17:29:00 -0600

    Added AutocompleteGUI
    Added Princeton's code for AutocompleteGUI

--- src/autocomplete/Autocomplete.java
+++ src/autocomplete/Autocomplete.java
@@ -20,6 +20,42 @@ public class Autocomplete {
 		Term t12 = new Term("for all my life", 732);
 		Term[] terms = {t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12};
 		Autocomplete autocomplete = new Autocomplete(terms);
+		Term[] testTerms = autocomplete.allMatches("");
+		for(Term t : testTerms) System.out.println(t);
+	}
+
+    // Initialize the data structure from the given array of terms.
+    public Autocomplete(Term[] terms){
+    	if(terms == null) throw new NullPointerException();
+    	this.TERMS = terms;
+    	Arrays.sort(this.TERMS);
+    }
+
+    // Return all terms that start with the given prefix, in descending order of weight.
+    public Term[] allMatches(String prefix){
+    	if(prefix == null) throw new NullPointerException();
+    	Term[] returnedTerms = new Term[numberOfMatches(prefix)];
+    	Term comparedTerm = new Term(prefix, 0);
+    	int termIndex = BinarySearchDeluxe.firstIndexOf(TERMS, comparedTerm, Term.byPrefixOrder(prefix.length()));
+    	
+    	for(int i = 0; i < returnedTerms.length; i++){
+    		returnedTerms[i] = TERMS[termIndex + i];
+    	}
+    	Arrays.sort(returnedTerms, Term.byReverseWeightOrder());
+    	return returnedTerms;
+    }
+
+    // Return the number of terms that start with the given prefix.
+    public int numberOfMatches(String prefix){
+    	if(prefix == null) throw new NullPointerException();
+    	Arrays.sort(TERMS, Term.byPrefixOrder(prefix.length()));
+    	//The temporary term for searching purposes.
+    	Term comparedTerm = new Term(prefix, 0);
+    	int lastIndex = BinarySearchDeluxe.lastIndexOf(TERMS, comparedTerm, Term.byPrefixOrder(prefix.length()));
+    	int firstIndex = BinarySearchDeluxe.firstIndexOf(TERMS, comparedTerm, Term.byPrefixOrder(prefix.length()));
+    	int numOfMatches = (lastIndex - firstIndex) + 1;
+    	//Account for it not existing
+    	if(lastIndex == -1 || firstIndex == -1) numOfMatches = 0;
     	return numOfMatches;
     }
 }
